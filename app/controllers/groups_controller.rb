3500cf0a5bc29d33f8c62c1dbcd2078ea5daefc1
bob <bao309@163.com>
2017-01-01 22:14:37 +0800

    implement groups#update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,6 +21,14 @@ def create
   redirect_to groups_path
 end
 
+def update
+  @group = Group.find(params[:id])
+
+  @group.update(group_params)
+
+  redirect_to group_path, notice: "update success"
+end
+
 private
 def group_params
   params.require(:group).permit(:title, :description)
